a491f18252859559487f6e6ae6cd6b8ff90c6adf
javawale <walejava@gmail.com>
2017-05-23 05:57:24 +0530

    Add files via upload

---
+++ Overloading.java
@@ -0,0 +1,13 @@
+class Overloading {
+	public String getMessage(char c) {
+		return "Hello World";
+	}
+
+	public Object getMessage() {
+		return "Hello";
+	}
+	
+	public static void main(String args[]) {
+		System.out.println("ABc");
+	}
+}
